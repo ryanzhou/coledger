f5c09cab60684cb41621ef7f64b2b2dc34769b77
Ryan Zhou <git@zhoutong.com>
2015-03-16 19:33:25 +1100

    fix broken tests

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -10,7 +10,7 @@ describe "Sessions Requests" do
       it "signs in the user and returns a new token" do
         post_json "/sessions", { username: "test_user", password: "password001" }
         expect(last_response.status).to eq(200)
-        expect(json["session"]["token"]).to be_a(String)
+        expect(json["token"]).to be_a(String)
       end
     end
 
@@ -30,7 +30,7 @@ describe "Sessions Requests" do
       it "returns the current session information" do
         get_json "/sessions/current"
         expect(last_response.status).to eq(200)
-        expect(json["session"]["id"]).to be_a(String)
+        expect(json["id"]).to be_a(String)
       end
     end
 
