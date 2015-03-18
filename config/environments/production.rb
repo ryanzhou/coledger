07e6a59d47355f35d580a3901e8837649d325ca8
Ryan Zhou <git@zhoutong.com>
2015-03-18 21:15:00 +1100

    do not mangle variable names

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,11 +25,8 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
-  # Do not mangle variables due to Angular's injector
   config.assets.js_compressor = Uglifier.new(mangle: false)
+  # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
